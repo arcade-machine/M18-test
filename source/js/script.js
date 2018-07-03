aa9c5a12b8c4bf06b9b7f33d3c5fc560b89d3a08
Сергей <agrael@yandex.ru>
2018-07-03 16:41:16 +0300

    Завершение верстки. Исправление недочетов

--- source/js/script.js
+++ source/js/script.js
@@ -21,3 +21,87 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+//-----------Для формы----------------//
+
+var form = document.querySelector(".main-form");
+
+var inputName = form.querySelector(".main-form__input--name");
+var inputEmail = form.querySelector(".main-form__input--email");
+var inputPhone = form.querySelector(".main-form__input--phone");
+
+var legendName = form.querySelector(".main-form__legend--name");
+var legendEmail = form.querySelector(".main-form__legend--email");
+var legendPhone = form.querySelector(".main-form__legend--phone");
+
+var fieldsetName = form.querySelector(".main-form__fieldset--name");
+var fieldsetEmail = form.querySelector(".main-form__fieldset--email");
+var fieldsetPhone = form.querySelector(".main-form__fieldset--phone");
+
+//-----------Легенда появляется на фокусе инпута----------------//
+
+var inputOnFocus = function(input, legend, fieldset) {
+    input.addEventListener("focus", function (evt) {
+        evt.preventDefault();
+        legend.classList.remove("hidden");
+        fieldset.style.border = "0.05vw solid #f9572e";
+    });
+    input.addEventListener("invalid", function (evt) {
+        evt.preventDefault();
+        legend.classList.remove("hidden");
+        fieldset.style.border = "0.05vw solid #f9572e";
+    });
+};
+
+inputOnFocus(inputName, legendName, fieldsetName);
+inputOnFocus(inputEmail, legendEmail, fieldsetEmail);
+inputOnFocus(inputPhone, legendPhone, fieldsetPhone);
+
+//-----------Легенда убирается, если инпут не в фокусе----------------//
+
+var inputOutOfFocus = function(input, legend, fieldset) {
+    input.addEventListener("focusout", function (evt) {
+        evt.preventDefault();
+        legend.classList.add("hidden");
+        fieldset.style.border = "0.05vw solid #ebebeb";
+    });
+};
+
+inputOutOfFocus(inputName, legendName, fieldsetName);
+inputOutOfFocus(inputEmail, legendEmail, fieldsetEmail);
+inputOutOfFocus(inputPhone, legendPhone, fieldsetPhone);
+
+//-----------Слайдер----------------//
+
+var sliderOneRadio = document.querySelector(".slider-controls__radio-button--slider-one");
+var sliderTwoRadio = document.querySelector(".slider-controls__radio-button--slider-two");
+var sliderThreeRadio = document.querySelector(".slider-controls__radio-button--slider-three");
+var sliderControls = document.querySelector(".slider-controls");
+var leftControl = sliderControls.querySelector(".slider-controls__item--left");
+var rightControl = sliderControls.querySelector(".slider-controls__item--right");
+
+leftControl.addEventListener('click', function () {
+    if (sliderTwoRadio.checked) {
+        sliderTwoRadio.removeAttribute('checked');
+        sliderOneRadio.setAttribute('checked', 'true');
+    } else if (sliderOneRadio.checked) {
+        sliderOneRadio.removeAttribute('checked');
+        sliderThreeRadio.setAttribute('checked', 'true');
+    } else if (sliderThreeRadio.checked) {
+        sliderThreeRadio.removeAttribute('checked');
+        sliderTwoRadio.setAttribute('checked', 'true');
+    }
+});
+rightControl.addEventListener('click', function () {
+    if (sliderTwoRadio.checked) {
+        sliderTwoRadio.removeAttribute('checked');
+        sliderThreeRadio.setAttribute('checked', 'true');
+    } else if (sliderOneRadio.checked) {
+        sliderOneRadio.removeAttribute('checked');
+        sliderTwoRadio.setAttribute('checked', 'true');
+    } else if (sliderThreeRadio.checked) {
+        sliderThreeRadio.removeAttribute('checked');
+        sliderOneRadio.setAttribute('checked', 'true');
+    }
+});
+
